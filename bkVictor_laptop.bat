8b90ee9b73a05e102cecdb88a3fbab87f52e88a1
Victor E. Torres T <victortorrestejada@gmail.com>
2023-04-12 17:28:29 -0500

    Remove .bat files from remote repository

--- bkVictor_laptop.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Archivos de programa\WinRAR\rar" u -dh -y -r -agYYYYMMDDHHMM k:\bk\bkaplHP_Prodesk  K:\aplvfp  -ta2020 -pjata16052009
-rem"d:\Program Files\WinRAR\rar" u -dh -y -r -agYYYYMMDDHHMM k:\bk\bkDatLapTop H:\o-negocios\aromas -ta20060830
