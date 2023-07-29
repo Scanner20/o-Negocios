7fbfda15179010759a68f6b53da7693ab9e7cb5b
Victor E. Torres T <victortorrestejada@gmail.com>
2023-07-29 16:28:52 -0500

    Mantenimiento 3

--- Classgen/Textos/clasesgenerales.h
+++
@@ -1,3 +0,0 @@
-#INCLUDE "H:\TDV\ClasesGenerales\Programacion01\Programacion\Textos\MSGSYS.H"
-#INCLUDE "H:\TDV\ClasesGenerales\Programacion01\Programacion\Textos\TDV_RUTAS.H"
-#INCLUDE "H:\TDV\ClasesGenerales\Programacion01\Programacion\Textos\REGISTRY.H"
